e26136e3fdfef025a978dba07ff80462fae67ba3
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-03-11 16:33:38 -0300

    fix: player profile motm

--- src/repositories/PostgresPlayerProfileRepository.ts
+++ src/repositories/PostgresPlayerProfileRepository.ts
@@ -15,6 +15,7 @@ export class PostgresPlayerProfileRepository implements IPlayerProfileRepository
               Game: true,
             },
           },
+          MOTM: true,
         },
       });
 
