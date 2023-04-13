a521c58363bd8f0ad60387f4a9e3f0c8213f10e2
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-04-13 13:38:03 -0300

    feature: add option to select player function on game

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,40 @@
 import { PlayerProfile } from './../src/entities/PlayerProfile';
 import { PrismaClient } from '@prisma/client'
+import {faker} from '@faker-js/faker'
+import {slugify} from '../../backend/src/functions/slugfy'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  const {players} = await prisma.game.findUniqueOrThrow({
-    where:{
-      id:22
-    },
-    include:{
-      players:true
+  await prisma.playerProfile.create({
+    data:{
+      name:'Goleiro-2',
+      slug:slugify('Goleiro-2'),
+      victories:0,
+      defeats:0,
+      draws:0,
+      assists:0,
+      goals:0,
+      function:'GOALKEEPER',
+      role:'PERMANENT',
+      shirtNumber:faker.datatype.number({min:1,max:99})
     }
-  })
+})
 
-  for await(const jogador of players){
-
-    if(jogador.currentTeam === 'GREEN'){
-      const pl = await prisma.playerProfile.findUniqueOrThrow({
-        where:{
-          id:jogador.playerId
-        }
-      })
-      console.log(pl)
-    }
-    return
+await prisma.playerProfile.create({
+  data:{
+    name:'Goleiro-1',
+    slug:slugify('Goleiro-1'),
+    victories:0,
+    defeats:0,
+    draws:0,
+    assists:0,
+    goals:0,
+    function:'GOALKEEPER',
+    role:'PERMANENT',
+    shirtNumber:faker.datatype.number({min:1,max:99})
   }
+})
 }
 main()
   .then(async () => {
